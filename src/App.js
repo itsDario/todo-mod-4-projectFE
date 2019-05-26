53a80faffd9682444c2592060119bbbf8aa1be50
jwy5140 <jwy5140@gmail.com>
2019-05-26 16:35:07 -0400

    Adjusted CSS, finished menu dropdown.

--- src/App.js
+++ src/App.js
@@ -11,19 +11,34 @@ export default class App extends React.Component {
     today: Date(Date.now()),
     menuBtn: false,
     usrName: 'User',
+    sidebar: false,
+  }
+
+  componentDidMount(){
+    this.interval = setInterval(()=>{this.setState({today: Date(Date.now())})}, 1000)
   }
   
-    hamburgerBtn = () => {
-      this.setState({
-        menuBtn: !this.state.menuBtn
-      })
+  hamburgerBtn = () => {
+    this.setState({
+      menuBtn: !this.state.menuBtn,
+      sidebar: !this.state.sidebar
+    })
+  }
+
+  openSidebar = () => {
+    if (this.state.sidebar === false){
+      return null
     }
+    else {
+      return < Sidebar /> 
+    }
+  }
 
   render(){
     return (
       <div className="App">
       < Navbar hamburgerBtn={this.hamburgerBtn} menuBtnState={this.state.menuBtn} usrName={this.state.usrName} />
-      < Sidebar />
+      {this.openSidebar()}
       < Calendar today={this.state.today} /> 
       </div>
     );
