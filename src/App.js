2d914b6d2fd205513b2afe202249d24e092b8887
jwy5140 <jwy5140@gmail.com>
2019-05-26 03:02:06 -0400

    added non-func Navbar, set up calendar.

--- src/App.js
+++ src/App.js
@@ -2,33 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Navbar from './containers/navbar'
-import Calendar from './containers/calendar'
-
-export default class App extends React.Component {
-
-  state = {
-    menuBtn: false,
-    usrName: 'blah',
-    year: '',
-    month: '',
-    day: '',
-  }
-
-  hamburgerBtn = () => {
-    this.setState({
-      menuBtn: !this.state.menuBtn
-    })
-  }
-
-  render(){
-    return (
-      <div className="App">
-        < Navbar hamburgerBtn={this.hamburgerBtn} menuBtnState={this.state.menuBtn} usrName={this.state.usrName}/>
-        < Calendar year={this.state.year} month={this.state.month} day={this.state.day} />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-
+export default App;
