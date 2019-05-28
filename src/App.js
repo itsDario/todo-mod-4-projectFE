973427ffd93959dafc91c5c1d2eaa2ff149b10ad
github username <itsDario1@gmail.com>
2019-05-28 12:21:26 -0400

    fixed merge bug

--- src/App.js
+++ src/App.js
@@ -7,69 +7,76 @@ import DayDock from './components/daydock'
 
 export default class App extends React.Component {
 
-  state = {
-    today: Date(Date.now()),
-    menuBtn: false,
-    user: {
-      name: 'Pidgey',
-    },
-    events: [{
-      id: 1,
-      name: 'Terrorize New Pokemon Trainers',
-      date: 5000000,
-      desc: 'win every match',
-      calender: 1,
-      user: 1
-    }, {
-      id: 2,
-      name: 'Event 2',
-      date: 50000000,
-      desc: 'win every match',
-      calender: 1,
-      user: 1
-    }, {
-      id: 3,
-      name: 'Event 3',
-      date: 500000000,
-      desc: 'win every match',
-      calender: 1,
-      user: 1
-    }, {
-      id: 4,
-      name: 'Event 4',
-      date: 500000000,
-      desc: 'win every match',
-      calender: 1,
-      user: 1
-    }, {
-      id: 5,
-      name: 'Event 5',
-      date: 5000000000,
-      desc: 'win every match',
-      calender: 1,
-      user: 1
-    }],
-    calendars: [{
-      id: 1,
-      name: 'Personal Calendar'
-    }, {
-      id: 2,
-      name: 'General Workspace Calendar'
-    }, {
-      id: 3,
-      name: 'Project 1 Calendar'
-    }, {
-      id: 4,
-      name: 'Project 2 Calendar'
-    }, {
-      id: 5,
-      name: 'Pidgey Mating Season'
-    }],
-    sidebar: false,
-    createCalendar: false,
-    daydock: false,
-    spotlight: '',
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      today: Date(Date.now()),
+      menuBtn: false,
+      user: {
+        name: 'Pidgey',
+      },
+      events: [{
+        id: 1,
+        name: 'Terrorize New Pokemon Trainers',
+        date: 5000000,
+        desc: 'win every match',
+        calender: 1,
+        user: 1
+      }, {
+        id: 2,
+        name: 'Event 2',
+        date: 50000000,
+        desc: 'win every match',
+        calender: 1,
+        user: 1
+      }, {
+        id: 3,
+        name: 'Event 3',
+        date: 500000000,
+        desc: 'win every match',
+        calender: 1,
+        user: 1
+      }, {
+        id: 4,
+        name: 'Event 4',
+        date: 500000000,
+        desc: 'win every match',
+        calender: 1,
+        user: 1
+      }, {
+        id: 5,
+        name: 'Event 5',
+        date: 5000000000,
+        desc: 'win every match',
+        calender: 1,
+        user: 1
+      }],
+      calendars: [{
+        id: 1,
+        name: 'Personal Calendar'
+      }, {
+        id: 2,
+        name: 'General Workspace Calendar'
+      }, {
+        id: 3,
+        name: 'Project 1 Calendar'
+      }, {
+        id: 4,
+        name: 'Project 2 Calendar'
+      }, {
+        id: 5,
+        name: 'Pidgey Mating Season'
+      }],
+      sidebar: false,
+      createCalendar: false,
+      daydock: false,
+      spotlight: '',
+    }
+
   }
+
 
   componentDidMount() {
     this.interval = setInterval(() => { this.setState({ today: Date(Date.now()) }) }, 1000)
@@ -179,17 +186,18 @@ export default class App extends React.Component {
     this.setState({
       calendars: [...this.state.calendars, { id: this.state.calendars.length + 1, name: event.target.calendarName.value }]
     })
-
-    render(){
-      return (
-        <div className="App">
-          < Navbar hamburgerBtn={this.hamburgerBtn} menuBtnState={this.state.menuBtn} username={this.state.user.name} />
-          {this.openSidebar()}
-          {this.openDayDock()}
-          < Calendar today={this.state.today} toggleDayDock={this.toggleDayDock} />
-        </div>
-      );
-    }
   }
+
+  render() {
+    return (
+      <div className="App">
+        < Navbar hamburgerBtn={this.hamburgerBtn} menuBtnState={this.state.menuBtn} username={this.state.user.name} />
+        {this.openSidebar()}
+        {this.openDayDock()}
+        < Calendar today={this.state.today} toggleDayDock={this.toggleDayDock} />
+      </div>
+    );
+  }
+}
 
 //use spotlight state to highlight day being viewed
