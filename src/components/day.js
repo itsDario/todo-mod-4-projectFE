45568b555dd946dfbc90879d66034cdd15060981
github username <itsDario1@gmail.com>
2019-05-29 16:01:09 -0400

    show the event names in the calendar entry

--- src/components/day.js
+++ src/components/day.js
@@ -6,12 +6,12 @@ export default class Day extends React.Component {
         return <span className={this.props.focus ? 'focus' : 'day'} id={this.props.day.id}>
             {this.props.day.num}
             <br />
-            <span className='eventName'>{this.props.events.length !== 0 ? `${this.props.events.length} Events` : ' '}</span>
+            <span className='eventName'>{this.props.events.length !== 0 ? this.eventMap() : ' '}</span>
         </span>
 
     }
 
     eventMap() {
-        return this.props.events.map(e => <div className='eventName'>{e.name}</div>)
+        return this.props.events.map((e, i) => i < 6 ? <div className='eventName'>{e.name}</div> : <span></span>)
     }
 }
