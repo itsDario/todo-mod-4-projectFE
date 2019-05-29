e0e3f6d3603e3f5af7b379b991d3127ff45fd77a
github username <itsDario1@gmail.com>
2019-05-29 13:57:20 -0400

    made boutton on month select line up

--- src/containers/monthselector.js
+++ src/containers/monthselector.js
@@ -8,24 +8,24 @@ const MonthSelector = (props) => {
     return <div className='monthselector'>
 
         <div>
-            <button name='January' onClick={e => props.setMonth(e.target.name)}>Jan</button>
-            <button name='February' onClick={e => props.setMonth(e.target.name)}>Feb</button>
-            <button name='March' onClick={e => props.setMonth(e.target.name)}>Mar</button>
+            <button className='monthButton' name='January' onClick={e => props.setMonth(e.target.name)}>Jan</button>
+            <button className='monthButton' name='February' onClick={e => props.setMonth(e.target.name)}>Feb</button>
+            <button className='monthButton' name='March' onClick={e => props.setMonth(e.target.name)}>Mar</button>
         </div>
         <div>
-            <button name='April' onClick={e => props.setMonth(e.target.name)}>Apr</button>
-            <button name='May' onClick={e => props.setMonth(e.target.name)}> May</button>
-            <button name='June' onClick={e => props.setMonth(e.target.name)}>Jun</button>
+            <button className='monthButton' name='April' onClick={e => props.setMonth(e.target.name)}>Apr</button>
+            <button className='monthButton' name='May' onClick={e => props.setMonth(e.target.name)}> May</button>
+            <button className='monthButton' name='June' onClick={e => props.setMonth(e.target.name)}>Jun</button>
         </div>
         <div>
-            <button name='July' onClick={e => props.setMonth(e.target.name)}> Jul</button>
-            <button name='August' onClick={e => props.setMonth(e.target.name)}> Aug</button>
-            <button name='September' onClick={e => props.setMonth(e.target.name)}> Sep</button>
+            <button className='monthButton' name='July' onClick={e => props.setMonth(e.target.name)}> Jul</button>
+            <button className='monthButton' name='August' onClick={e => props.setMonth(e.target.name)}> Aug</button>
+            <button className='monthButton' name='September' onClick={e => props.setMonth(e.target.name)}> Sep</button>
         </div>
         <div>
-            <button name='October' onClick={e => props.setMonth(e.target.name)}> Oct</button>
-            <button name='November' onClick={e => props.setMonth(e.target.name)}> Nov</button>
-            <button name='December' onClick={e => props.setMonth(e.target.name)}> Dec</button>
+            <button className='monthButton' name='October' onClick={e => props.setMonth(e.target.name)}> Oct</button>
+            <button className='monthButton' name='November' onClick={e => props.setMonth(e.target.name)}> Nov</button>
+            <button className='monthButton' name='December' onClick={e => props.setMonth(e.target.name)}> Dec</button>
         </div>
     </div>
 }
